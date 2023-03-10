94f5d6f51709190fc69b9df9ad8733daaff7a6c3
Nguyen Duc Nam <namworkmc@gmail.com>
2023-03-10 10:31:14 +0700

    [DOC-146] Setup react-i18n

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import { LoginForm, ProConfigProvider, ProFormText } from '@ant-design/pro-components';
 import logo from 'assets/icons/logo.png';
+import axios from 'axios';
 import { useAuth } from 'components/AuthComponent';
 import { getToken } from 'services/SecurityService';
 import { getCurrentUser } from 'services/UserService';
@@ -12,6 +13,7 @@ const I18N_PREFIX = 'LOGIN';
 const LoginPage: React.FC = () => {
   const { saveAuth, setCurrentUser } = useAuth();
   const { t } = useTranslation();
+  const [error, setError] = useState<string>();
 
   const handleOnFinish = async (values: Record<string, string>) => {
     try {
@@ -20,7 +22,12 @@ const LoginPage: React.FC = () => {
       const { data: user } = await getCurrentUser();
       setCurrentUser(user);
     } catch (e) {
-      console.error(e);
+      if (axios.isAxiosError(e)) {
+        setError(e.response?.data.message);
+        console.error(e.response?.data.message);
+      } else {
+        console.error(e);
+      }
     }
   };
 
@@ -39,29 +46,35 @@ const LoginPage: React.FC = () => {
           onFinish={handleOnFinish}>
           <ProFormText
             name='username'
+            hasFeedback={!!error}
+            help={t(error as string)}
+            validateStatus={error ? 'error' : 'success'}
             fieldProps={{
               size: 'large',
               prefix: <UserOutlined className='prefixIcon' />,
             }}
-            placeholder={t(`${I18N_PREFIX}.USERNAME.PLACEHOLDER`) as string}
+            placeholder={t(`${I18N_PREFIX}.USERNAME.PLACEHOLDER`).toString()}
             rules={[
               {
                 required: true,
-                message: t(`${I18N_PREFIX}.USERNAME.RULE_MESSAGE`) as string,
+                message: t(`${I18N_PREFIX}.USERNAME.RULE_MESSAGE`).toString(),
               },
             ]}
           />
           <ProFormText.Password
             name='password'
+            hasFeedback={!!error}
+            help={t(error as string)}
+            validateStatus={error ? 'error' : 'success'}
             fieldProps={{
               size: 'large',
               prefix: <LockOutlined className='prefixIcon' />,
             }}
-            placeholder={t(`${I18N_PREFIX}.PASSWORD.PLACEHOLDER`) as string}
+            placeholder={t(`${I18N_PREFIX}.PASSWORD.PLACEHOLDER`).toString()}
             rules={[
               {
                 required: true,
-                message: t(`${I18N_PREFIX}.PASSWORD.RULE_MESSAGE`) as string,
+                message: t(`${I18N_PREFIX}.PASSWORD.RULE_MESSAGE`).toString(),
               },
             ]}
           />
