61a2e71003b06f1e97887427a5a569fad6486c59
Hoang Huu Giap <hoanghuugiap241@gmail.com>
2023-03-29 21:02:28 +0700

    [DOC-112] Update SiteController

--- src/models/doc-main-models.ts
+++ src/models/doc-main-models.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 3.1.1185 on 2023-03-29 00:57:33.
+// Generated using typescript-generator version 3.1.1185 on 2023-03-29 19:35:35.
 
 export interface DistributionOrganizationDto extends DocAbstractDto {
     name: string;
