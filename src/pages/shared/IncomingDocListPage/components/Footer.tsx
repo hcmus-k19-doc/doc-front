3a6559e1682bc57e90e21fb98ea2160d4f5382bf
Hoang Huu Giap <hoanghuugiap241@gmail.com>
2023-05-01 18:41:46 +0700

    [DOC-171] Move transfer document button to Footer.tsx component

--- src/pages/shared/IncomingDocListPage/components/Footer.tsx
+++ src/pages/shared/IncomingDocListPage/components/Footer.tsx
@@ -1,20 +1,112 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Button, Pagination } from 'antd';
+import { useForm } from 'antd/es/form/Form';
+import axios from 'axios';
+import { useAuth } from 'components/AuthComponent';
+import TransferDocModal from 'components/TransferDocModal';
+import { TransferDocDto } from 'models/doc-main-models';
+import { useRecoilValue } from 'recoil';
+import incomingDocumentService from 'services/IncomingDocumentService';
 import { useIncomingDocReq, useIncomingDocRes } from 'shared/hooks/IncomingDocumentListQuery';
+import { useSweetAlert } from 'shared/hooks/SwalAlert';
+import { initialTransferQueryState, useTransferQuerySetter } from 'shared/hooks/TransferDocQuery';
+import { validateTransferDocs } from 'shared/validators/TransferDocValidator';
 
-const Footer = () => {
+import { getSelectedDocsMessage } from '../core/common';
+import { FooterProps } from '../core/models';
+import { transferDocModalState } from '../core/states';
+
+const Footer: React.FC<FooterProps> = ({ selectedDocs, setSelectedDocs }) => {
   const { t } = useTranslation();
   const [incomingDocReqQuery, setIncomingDocReqQuery] = useIncomingDocReq();
   const { data } = useIncomingDocRes();
-
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalForm] = useForm();
+  const { currentUser } = useAuth();
+  const [, setError] = useState<string>();
+  const showAlert = useSweetAlert();
+  const transferDocModalItem = useRecoilValue(transferDocModalState);
+  const transferQuerySetter = useTransferQuerySetter();
   const handleOnChange = (page: number, pageSize: number) => {
+    setSelectedDocs([]);
     setIncomingDocReqQuery({ ...incomingDocReqQuery, page, pageSize });
   };
 
+  const handleOnOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOnCancelModal = () => {
+    setIsModalOpen(false);
+    modalForm.resetFields();
+    transferQuerySetter(initialTransferQueryState);
+  };
+
+  const handleOnOkModal = async () => {
+    const transferDocDto: TransferDocDto = {
+      documentIds: selectedDocs.map((doc) => doc.id),
+      summary: modalForm.getFieldValue('summary'),
+      reporterId: currentUser?.id as number,
+      assigneeId: modalForm.getFieldValue('assignee') as number,
+      collaboratorIds: modalForm.getFieldValue('collaborators') as number[],
+      processingTime: modalForm.getFieldValue('processingTime'),
+      isInfiniteProcessingTime: modalForm.getFieldValue('isInfiniteProcessingTime'),
+      processMethod: modalForm.getFieldValue('processMethod'),
+      transferDocumentType: transferDocModalItem,
+    };
+    if (validateTransferDocs(selectedDocs, transferDocModalItem, transferDocDto, t)) {
+      setIsModalOpen(false);
+      modalForm.submit();
+      console.log(modalForm.getFieldsValue());
+      modalForm.resetFields();
+      transferQuerySetter(transferDocDto);
+      console.log(transferDocDto, transferDocModalItem);
+      try {
+        const response = await incomingDocumentService.transferDocuments(transferDocDto);
+        console.log(response);
+        if (response.status === 200) {
+          // TODO: refetch data
+          showAlert({
+            icon: 'success',
+            html: t('incomingDocListPage.message.transfer_success') as string,
+            showConfirmButton: false,
+            timer: 2000,
+          });
+        }
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          setError(error.response?.data.message);
+          console.error(error.response?.data.message);
+        } else {
+          console.error(error);
+        }
+      }
+      setSelectedDocs([]);
+    }
+  };
+  const hasSelected = selectedDocs.length > 0;
+
   return (
     <div className='mt-5 flex justify-between'>
-      <Button type='primary'>{t('MAIN_PAGE.BUTTON.REPORT_TO_LEADER')}</Button>
+      <div className='float-left transfer-doc-wrapper'>
+        <Button
+          type='primary'
+          onClick={handleOnOpenModal}
+          className='transfer-doc-btn'
+          disabled={!hasSelected}>
+          {t('incomingDocDetailPage.button.transfer')}
+        </Button>
+
+        <span style={{ marginTop: 8 }}>
+          {hasSelected
+            ? t('incomingDocListPage.message.selected_docs.summary', {
+                count: hasSelected ? selectedDocs.length : 0,
+                ...getSelectedDocsMessage(selectedDocs, t),
+              })
+            : ''}
+        </span>
+      </div>
 
       <Pagination
         current={incomingDocReqQuery.page}
@@ -22,6 +114,14 @@ const Footer = () => {
         onChange={handleOnChange}
         total={data?.totalElements}
         showTotal={(total) => t('COMMON.PAGINATION.SHOW_TOTAL', { total })}
+      />
+
+      <TransferDocModal
+        form={modalForm}
+        isModalOpen={isModalOpen}
+        handleCancel={handleOnCancelModal}
+        handleOk={handleOnOkModal}
+        selectedDocs={selectedDocs}
       />
     </div>
   );
