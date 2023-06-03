af292c801664a8e3b2d55a6763ba067ca55cfd3e
Hoang Huu Giap <hoanghuugiap241@gmail.com>
2023-06-03 22:24:09 +0700

    [DOC-202] Update code

--- src/models/doc-main-models.ts
+++ src/models/doc-main-models.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 3.1.1185 on 2023-06-03 21:56:11.
+// Generated using typescript-generator version 3.1.1185 on 2023-06-03 22:17:17.
 
 export interface CommentDto extends DocAbstractDto {
     content: string;
