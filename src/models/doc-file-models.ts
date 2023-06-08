4274b6b53fbc130035d9bddf2500deca4dfaffa7
Hoang Huu Giap <hoanghuugiap241@gmail.com>
2023-06-09 00:23:27 +0700

    [DOC-218] Fix code for outgoing transfer detail

--- src/models/doc-file-models.ts
+++ src/models/doc-file-models.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 3.1.1185 on 2023-06-07 21:24:51.
+// Generated using typescript-generator version 3.1.1185 on 2023-06-08 22:11:53.
 
 export const enum ParentFolderEnum {
     ICD = "ICD",
