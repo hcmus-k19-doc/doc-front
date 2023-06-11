3dc357f142ffd88269693b571578a6bbc0b290d7
Thanh Hoang <thanhhoang4869@gmail.com>
2023-06-11 14:49:08 +0700

    [DOC-195] Set locale to frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { ConfigProvider } from 'antd';
+import vi_VN from 'antd/lib/locale/vi_VN';
 import { AuthInit } from 'components/AuthComponent';
 import { PRIMARY_COLOR } from 'config/constant';
 
@@ -9,6 +10,7 @@ import './App.css';
 const App: React.FC = () => {
   return (
     <ConfigProvider
+      locale={vi_VN}
       theme={{
         token: {
           colorPrimary: PRIMARY_COLOR,
