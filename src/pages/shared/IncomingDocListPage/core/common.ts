e33623543b68c78c33e2ce3ee8cbe2fed9ae00bd
Hoang Huu Giap <hoanghuugiap241@gmail.com>
2023-06-17 21:13:41 +0700

    Add status color for incoming document

--- src/pages/shared/IncomingDocListPage/core/common.ts
+++ src/pages/shared/IncomingDocListPage/core/common.ts
@@ -14,4 +14,17 @@ const getSelectedDocsMessage = (selectedDocs: IncomingDocumentDto[], t?: any) =>
   return { unprocessedDocs, processingDocs, closedDocs };
 };
 
-export { getSelectedDocsMessage };
+const getColorBaseOnStatus = (status: string, t: any) => {
+  switch (status) {
+    case t('PROCESSING_STATUS.UNPROCESSED'):
+      return 'gray';
+    case t('PROCESSING_STATUS.IN_PROGRESS'):
+      return 'green';
+    case t('PROCESSING_STATUS.CLOSED'):
+      return 'red';
+    default:
+      return 'gray';
+  }
+};
+
+export { getSelectedDocsMessage, getColorBaseOnStatus };
