d5a361fb4e529f2bce4a90a0928553dd66536f28
Hoang Huu Giap <hoanghuugiap241@gmail.com>
2023-06-17 20:45:04 +0700

    Add loading status

--- src/models/doc-main-models.ts
+++ src/models/doc-main-models.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 3.1.1185 on 2023-06-17 07:24:29.
+// Generated using typescript-generator version 3.1.1185 on 2023-06-17 20:14:37.
 
 export interface CommentDto extends DocAbstractDto {
     content: string;
