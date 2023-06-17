05e40cb5a67f2f0396b5eb90bade60abe1e56d36
Hoang Huu Giap <hoanghuugiap241@gmail.com>
2023-06-17 07:48:35 +0700

    [DOC-215] Add notification when transfer outgoing document

--- src/models/doc-file-models.ts
+++ src/models/doc-file-models.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 3.1.1185 on 2023-06-10 00:15:42.
+// Generated using typescript-generator version 3.1.1185 on 2023-06-17 07:24:21.
 
 export const enum ParentFolderEnum {
     ICD = "ICD",
