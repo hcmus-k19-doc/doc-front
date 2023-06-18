275d76e85679b044bfa1029ed6a358ddd1f699d8
Hoang Huu Giap <hoanghuugiap241@gmail.com>
2023-06-18 16:49:29 +0700

    [DOC-223] Refactor transfer history function

--- src/models/doc-main-models.ts
+++ src/models/doc-main-models.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 3.1.1185 on 2023-06-18 16:09:34.
+// Generated using typescript-generator version 3.1.1185 on 2023-06-18 16:27:00.
 
 export interface CommentDto extends DocAbstractDto {
     content: string;
