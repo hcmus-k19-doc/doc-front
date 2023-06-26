a09b1c501c335f99215cd9a9431c380840b708cf
Hoang Huu Giap <hoanghuugiap241@gmail.com>
2023-06-26 23:25:00 +0700

    [DOC-228] Add fileName to attachmentDto

--- src/models/doc-file-models.ts
+++ src/models/doc-file-models.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 3.1.1185 on 2023-06-20 20:59:38.
+// Generated using typescript-generator version 3.1.1185 on 2023-06-26 23:22:12.
 
 export const enum ParentFolderEnum {
     ICD = "ICD",
