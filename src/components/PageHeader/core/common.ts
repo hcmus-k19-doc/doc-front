7bcf26b9ea587042c6203fd5c54a45709ed407fd
Hoang Huu Giap <hoanghuugiap241@gmail.com>
2023-07-06 00:08:27 +0700

    [DOC-228] Fix FE sort notification in wrong order

--- src/components/PageHeader/core/common.ts
+++ src/components/PageHeader/core/common.ts
@@ -7,9 +7,11 @@ export const joinArrayWithoutDuplicate = (oldArray: any[], newArray: any[]) => {
     if (!result.find((i) => i.id === item.id)) {
       result.push(item);
     }
-    // if (!result.includes(item)) {
-    //   result.push(item);
-    // }
   });
+  // sort result by newest createdDate and if equal then compare createdTime
+  result.sort((a, b) => {
+    return b.id - a.id;
+  });
+
   return result;
 };
