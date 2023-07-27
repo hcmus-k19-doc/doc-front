06191393fdd6848f79683314108633168f45575d
Hoang Huu Giap <hoanghuugiap241@gmail.com>
2023-07-27 22:11:02 +0700

    [DOC-251] Complete return request function

--- src/models/doc-file-models.ts
+++ src/models/doc-file-models.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 3.1.1185 on 2023-07-25 22:16:53.
+// Generated using typescript-generator version 3.1.1185 on 2023-07-27 19:40:44.
 
 export const enum ParentFolderEnum {
     ICD = "ICD",
