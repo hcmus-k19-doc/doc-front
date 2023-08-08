bee182cbad34b7b309ecdcbc9560b66cb3e93391
hhgiap241 <hoanghuugiap241@gmail.com>
2023-08-08 22:18:46 +0700

    [DOC-277] Set up mail service

--- src/models/doc-file-models.ts
+++ src/models/doc-file-models.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 3.1.1185 on 2023-08-07 22:39:55.
+// Generated using typescript-generator version 3.1.1185 on 2023-08-08 20:35:31.
 
 export const enum ParentFolderEnum {
     ICD = "ICD",
