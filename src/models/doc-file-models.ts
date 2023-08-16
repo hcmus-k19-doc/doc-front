10a6165ff437edd7cea092c2f0334c2cfb5e20c1
hhgiap241 <hoanghuugiap241@gmail.com>
2023-08-16 21:04:34 +0700

    [DOC-278] Validate password

--- src/models/doc-file-models.ts
+++ src/models/doc-file-models.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 3.1.1185 on 2023-08-14 21:42:28.
+// Generated using typescript-generator version 3.1.1185 on 2023-08-16 20:18:03.
 
 export const enum ParentFolderEnum {
     ICD = "ICD",
